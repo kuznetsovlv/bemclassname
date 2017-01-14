ed958b9915b895ff765fd9bb1446d43f8c728cd4
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-01-15 02:08:19 +0300

    Set tests

--- src/tests/test_dev.js
+++ src/tests/test_dev.js
@@ -1,6 +1,43 @@
 import {Unit, Test}	from 'unit-kuznetsov';
-import bemClassName	from '../index';
+import bemClassNames	from '../index';
 
-const testUnit = new Unit (bemClassName, 'Check "exist"');
+const testUnit = new Unit (bemClassNames, 'Tests for bemClassNames');
 
-export default testUnit.addTest(new Test({arg: 'test', method: 'exist'}));
+export default testUnit
+						.addTest(new Test({args: ['block'], method: 'isEqual', expectation: 'block'}))
+						.addTest(new Test({args: ['BLOCK'], method: 'isEqual', expectation: 'block'}))
+						.addTest(new Test({args: ['block', 'element'], method: 'isEqual', expectation: 'block__element'}))
+						.addTest(new Test({args: ['bLock', 'ELEMENT'], method: 'isEqual', expectation: 'block__element'}))
+						.addTest(new Test({args: ['block', 'element', 'mod1'], method: 'isEqual', expectation: 'block__element block__element_mod1'}))
+						.addTest(new Test({args: ['block', 'element', 'moD1'], method: 'isEqual', expectation: 'block__element block__element_mod1'}))
+						.addTest(new Test({args: ['bloCk', 'element', 'mod1', 'mOd2'], method: 'isEqual', expectation: 'block__element block__element_mod1 block__element_mod2'}))
+						.addTest(new Test({args: ['bloCk', 'element', ['mod1', 'mOd2']], method: 'isEqual', expectation: 'block__element block__element_mod1 block__element_mod2'}))
+						.addTest(new Test({
+							args: [
+							'bloCk',
+							'element',
+							['mod1', 'mOd2'],
+							'mod3',
+							['mod4', 'mod5', 'mod6']
+							],
+							method: 'isEqual',
+							expectation: 'block__element block__element_mod1 block__element_mod2 block__element_mod3 block__element_mod4 block__element_mod5 block__element_mod6'
+						}))
+						.addTest(new Test({
+							args: [
+							'block',
+							'element',
+							{mod1: true, mod2: false, mOd3: 1}
+							],
+							method: 'isEqual',
+							expectation: 'block__element block__element_mod1 block__element_mod3'}))
+						.addTest(new Test({
+							args: [
+							'block',
+							'element',
+							{mod1: true, mod2: false, mOd3: 1, mod4: ['mod5', {mod6: true}]},
+							'mod7',
+							[{mod8: true}]
+							],
+							method: 'isEqual',
+							expectation: 'block__element block__element_mod1 block__element_mod3 block__element_mod5 block__element_mod6 block__element_mod7 block__element_mod8'}));
