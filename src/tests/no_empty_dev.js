e087e4f6a8668a62bc4a0e76b6a6317701354cb7
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-01-15 23:13:45 +0300

    Added tests for filtering empty modificators

--- src/tests/no_empty_dev.js
+++ src/tests/no_empty_dev.js
@@ -8,4 +8,28 @@ export default noEmpty
 						.addTest(new Test({args: ['', 'element'], method: 'isEqual', expectation: ''}))
 						.addTest(new Test({args: ['', 'element', 'mod'], method: 'isEqual', expectation: ''}))
 						.addTest(new Test({args: ['', '', 'mod'], method: 'isEqual', expectation: ''}))
-						.addTest(new Test({args: [], method: 'isEqual', expectation: ''}));
+						.addTest(new Test({args: [], method: 'isEqual', expectation: ''}))
+						.addTest(new Test({
+							args: [
+								'block',
+								'element',
+								'mod1',
+								''
+							],
+							method: 'isEqual',
+							expectation: 'block__element block__element_mod1'
+						}))
+						.addTest(new Test({
+							args: [
+								'block',
+								'element',
+								'mod1',
+								'',
+								'mod2',
+								null,
+								0,
+								'mod3'
+							],
+							method: 'isEqual',
+							expectation: 'block__element block__element_mod1 block__element_mod2 block__element_mod3'
+						}));
