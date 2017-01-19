86e0f12bc2e496d8a3a0d1c64aeee73d362e6bba
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-01-20 00:19:40 +0300

    Fixed wrong export into window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export default from './main';
+import bemclassnames	from './main';
+
+/*
+ * Changed export default to module.exports becase it is the only way to export bemclassname into the window object.
+ */
+module.exports = bemclassnames;
